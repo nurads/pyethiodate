3b9f3c18f55587e802582985037dad48ff50ff49
nurad <nuradhussen082@gmail.com>
2024-01-12 23:31:06 +0300

    exception handling

---
+++ pyethiodate/exceptions.py
@@ -0,0 +1,32 @@
+class InvalidYearException(Exception):
+    def __init__(self, message="Invalid year input") -> None:
+        self.message
+
+
+class InValidDayOfTheMonthException:
+    def __init__(
+        self, message="Invalid day of the month ethiopian date range 1...30"
+    ) -> None:
+        self.message = message
+
+
+class InValidMonthOfTheYearException:
+    def __init__(
+        self, message="Invalid Month Input: the input should be 1...12"
+    ) -> None:
+        self.message = message
+
+
+class InvalidHourException:
+    def __init__(self, message="Invalid Hour input") -> None:
+        pass
+
+
+class InvalidMinuteException:
+    def __init__(self, message="Invalid Minute input") -> None:
+        pass
+
+
+class InvalidDateException:
+    def __init__(self, message="Invalid Date Input") -> None:
+        self.message = message
