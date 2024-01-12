b560614dde0b9b4f78aa3723d7650346f64b797e
nurad <nuradhussen082@gmail.com>
2024-01-12 16:51:40 +0300

    setup tools

--- setup.py
+++ setup.py
@@ -1,34 +1,28 @@
 #!/usr/bin/env python
 # encoding=utf-8
-# maintainer: rgaudin
+# maintainer: Nuradic
 
 from __future__ import absolute_import
 from __future__ import unicode_literals
 import setuptools
 
 setuptools.setup(
-    name='ethiopian_date',
-    version='1.0',
-    license='GNU General Public License (GPL), Version 3',
-
-    provides=['ethiopian_date'],
-
-    description='Ethiopian date converter.',
-    long_description=open('README.rst').read(),
-
-    url='http://github.com/rgaudin/tools',
-
-    packages=['ethiopian_date'],
-
+    name="pythopia",
+    version="0.0.1",
+    license="GNU General Public License (GPL), Version 3",
+    provides=["ethiopian_date"],
+    description="Ethiopian date converter.",
+    long_description=open("README.rst").read(),
+    url="http://github.com/nuradic/pythopia",
+    packages=["ethiopian_date"],
     install_requires=[
-        'six>=1.11.0',
+        "six>=1.11.0",
     ],
-
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Library or '
-        'Lesser General Public License (LGPL)',
-        'Programming Language :: Python',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or "
+        "Lesser General Public License (LGPL)",
+        "Programming Language :: Python",
     ],
 )
